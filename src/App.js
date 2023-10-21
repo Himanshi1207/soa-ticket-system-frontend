1ee5b20ca938b2b25b6be9c5c616e257bd9b9875
Himanshi <kanojiahimanshi1221@gmail.com>
2023-10-21 13:20:04 +0530

    layout added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import { DefaultLayout } from './layout/DefaultLayout';
 import Entry from './pages/entry/entry.page';
 function App() {
   return (
     <div className="App">
-      <Entry/>
+      {/* <Entry/> */}
+      <DefaultLayout>Dashboard</DefaultLayout>
     </div>
   );
 }
