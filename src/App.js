86b7ef494a6f78206e6eea1fbbace4127a8e9237
Himanshi <kanojiahimanshi1221@gmail.com>
2023-10-21 21:21:48 +0530

    breadcrumb added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import './App.css';
-import { DefaultLayout } from './layout/DefaultLayout';
-import Entry from './pages/entry/entry.page';
+import "./App.css";
+import { DefaultLayout } from "./layout/DefaultLayout";
+import { Dashboard } from "./pages/dashboard/Dashboard.page";
+import Entry from "./pages/entry/entry.page";
 function App() {
   return (
     <div className="App">
       {/* <Entry/> */}
-      <DefaultLayout>Dashboard</DefaultLayout>
+      <DefaultLayout>
+        <Dashboard />
+      </DefaultLayout>
     </div>
   );
 }
--- src/layout/partials/Header.comp.js
+++ src/layout/partials/Header.comp.js
@@ -10,9 +10,15 @@ export const Header = () => {
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="ml-auto">
-          <Nav.Link href="/dashboard">Dashboard</Nav.Link>
-          <Nav.Link href="/dashboard">Tickets</Nav.Link>
-          <Nav.Link href="/dashboard">Logout</Nav.Link>
+          <Nav.Link style={{ color: "white" }} href="/dashboard">
+            Dashboard
+          </Nav.Link>
+          <Nav.Link style={{ color: "white" }} href="/dashboard">
+            Tickets
+          </Nav.Link>
+          <Nav.Link style={{ color: "white" }}  href="/dashboard">
+            Logout
+          </Nav.Link>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
