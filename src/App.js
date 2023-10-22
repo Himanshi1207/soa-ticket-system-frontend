ef03a66245399f09267d9c23c56e833e1ae01610
Himanshi <kanojiahimanshi1221@gmail.com>
2023-10-22 21:17:30 +0530

    ticket-landing-page added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Dashboard } from "./pages/dashboard/Dashboard.page";
 import Entry from "./pages/entry/entry.page";
 import { AddTicket } from "./pages/new-ticket/AddTicket.page";
 import { TicketLists } from "./pages/ticket-list/TicketLists.page";
+import { Ticket } from "./pages/ticket/Ticket.page";
 function App() {
   return (
     <div className="App">
@@ -11,7 +12,8 @@ function App() {
       <DefaultLayout>
         {/* <Dashboard /> */}
         {/* <AddTicket/> */}
-        <TicketLists/>
+        {/* <TicketLists/> */}
+        <Ticket/>
       </DefaultLayout>
     </div>
   );
